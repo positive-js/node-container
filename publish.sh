ac3ebcaa7c18d4dbadacb31ed2bbf7b59001c1d2
Oleg Pimenov <pimenov.o@gmail.com>
2017-09-22 11:21:07 +0300

    Create publish.sh

---
+++ publish.sh
@@ -0,0 +1,11 @@
+set -e
+
+TAG=$1
+if [ -z $TAG ]
+  then echo "usage: $0 [tag]"; exit 1
+fi
+
+docker build . -t ptsecurity/node-container:$TAG
+docker push ptsecurity/node-container
+git tag -a $TAG -m "published to docker"
+git push --tags
